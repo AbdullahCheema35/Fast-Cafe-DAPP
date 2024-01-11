75423757ed7167aaacd49c8c6038770523da8506
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-01-11 18:03:09 +0500

    Changed deployment script to initialize state; Set staff addressees, and get FastCoin for accounts[4-8]

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -58,4 +58,18 @@ module.exports = async function (deployer) {
             orderProcessingInstance.address
         )
     ]);
+
+    // Get all accounts
+    const accounts = await web3.eth.getAccounts();
+
+    // account[0] is the owner of the contract, account[1-3] are the staff addresses, account[4-9] are the customer addresses
+    const staffAddresses = [accounts[1], accounts[2], accounts[3]];
+
+    // Set the staff addresses in menuManagementInstance and promotionDiscountInstance
+    for (let i = 0; i < staffAddresses.length; i++) {
+        await Promise.all([
+            menuManagementInstance.addStaff(staffAddresses[i]),
+            promotionDiscountInstance.addStaff(staffAddresses[i]),
+        ]);
+    }
 };
--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -5,19 +5,19 @@ const PaymentContract = artifacts.require("FastCoin");
 module.exports = async function (deployer) {
     const paymentContractInstance = await PaymentContract.deployed();
     const accounts = await web3.eth.getAccounts();
-    const sender = accounts[5];
+
+    // sender are the accounts[4-8]
+    const sender = [accounts[4], accounts[5], accounts[6], accounts[7], accounts[8]];
 
     // const initialBalance = await web3.eth.getBalance(paymentContractInstance.address);
     const valueToSend = web3.utils.toWei("10", "ether");
 
     // Send 10 ethers directly to the receive() function of PaymentContract
-    await web3.eth.sendTransaction({
-        from: sender,
-        to: paymentContractInstance.address,
-        value: valueToSend,
-    });
-
-    const tokenBalance = await paymentContractInstance.balanceOf(sender);
-
-    console.log(tokenBalance.toString());
+    for (let i = 0; i < sender.length; i++) {
+        await web3.eth.sendTransaction({
+            from: sender[i],
+            to: paymentContractInstance.address,
+            value: valueToSend
+        });
+    }
 }
